28efefdeebed5548ee1764da23f6f5a262f9a141
Frederico Jordan <fredericojordan@gmail.com>
2020-04-17 23:44:05 -0300

    Remove logfiles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,18 +13,10 @@ else
     python src/manage.py migrate
     python src/manage.py collectstatic --noinput
 
-    # Prepare log files and start outputting logs to stdout
-    mkdir -p /srv/logs/
-    touch /srv/logs/gunicorn.log
-    touch /srv/logs/access.log
-    tail -n 0 -f /srv/logs/*.log &
-
     echo Starting Gunicorn
     exec gunicorn app.wsgi \
         --bind 0.0.0.0:8000 \
-        --chdir /usr/src/app/src \
+        --chdir /app/src \
         --workers 3 \
-        --log-level=info \
-        --log-file=/srv/logs/gunicorn.log \
-        --access-logfile=/srv/logs/access.log
+        --log-level=info
 fi
