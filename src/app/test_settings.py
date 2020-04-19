c4f84570c8c93ffab19dff13d4f698aaae1a39b6
Frederico Jordan <fredericojordan@gmail.com>
2020-04-19 18:40:57 -0300

    Fix test settings

--- src/app/test_settings.py
+++ src/app/test_settings.py
@@ -1,20 +1,7 @@
-from .settings import *
+from app.settings import *
 
 
 DJANGO_DEBUG = True
 ENVIRONMENT = "test"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test",
-        "HOST": "127.0.0.1",
-        "PORT": 5432,
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-    }
-}
-
-EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-
-PASSWORD_RESET_CONFIRM_PATH = "accouts/reset_password"
+DATABASES["default"]["NAME"] = "test"
