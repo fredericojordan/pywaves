de471095a090535d2fe2344beb1fad2dd615e991
Frederico Jordan <fredericojordan@gmail.com>
2020-04-24 11:56:34 -0300

    Add Forecasts button

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.contrib.gis.admin import OSMGeoAdmin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from core.models import Spot, Forecast
 
 
 class SpotAdmin(OSMGeoAdmin):
     # map_template = "opencyclemap.html"
-    actions = ["get_forecast"]
-    list_display = ["name", "latitude", "longitude"]
+    actions = ["generate_forecast"]
+    list_display = ["name", "latitude", "longitude", "get_forecasts"]
 
-    def get_forecast(self, request, queryset):
+    def get_forecasts(self, obj):
+        return format_html(
+            '<a class="button" href="{}?spot__id__exact={}">Forecasts ({})</a>&nbsp;',
+            reverse("admin:core_forecast_changelist"),
+            obj.id,
+            obj.forecasts.count(),
+        )
+
+    def generate_forecast(self, request, queryset):
         forecasts = []
         for spot in queryset:
             forecasts.append(spot.save_forecast())
@@ -25,24 +35,17 @@ class SpotAdmin(OSMGeoAdmin):
                 request, f"{len(forecasts)} forecasts have been generated!"
             )
 
-    get_forecast.short_description = "Fetch spot forecast via API and save results"
+    generate_forecast.short_description = "Fetch spot forecast via API and save results"
 
 
 class ForecastAdmin(ModelAdmin):
     readonly_fields = ("spot", "created", "raw_data")
     fieldsets = (
         (None, {"fields": ("spot", "created")}),
-        ("Raw Data", {"classes": ("collapse",), "fields": ("raw_data",)},),
+        ("Raw Data", {"classes": ("collapse",), "fields": ("raw_data",)}),
     )
-    list_filter = (
-        "spot",
-        "created",
-    )
-    list_display = (
-        "id",
-        "spot",
-        "created",
-    )
+    list_filter = ("spot", "created")
+    list_display = ("id", "spot", "created")
 
     change_form_template = "change_forecast.html"
 
